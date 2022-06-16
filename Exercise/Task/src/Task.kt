b4c8bbf3008a3c6668559c35a8317022597ea774
Mundurujoana <mundujoana@gmail.com>
2022-06-16 18:43:57 +0300

    mutable And immutable Variables

---
+++ Exercise/Task/src/Task.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // Example: Person
+    val name = "Bob"  // String
+    val age = 51 // Integer
+    var weight = 212.15 // Double
+    val isOrganDonor = false // Boolean
+
+    println(name)
+    println(age)
+    println(weight)
+    println(isOrganDonor)
+
+    // A variable's data is mutable (changeable)
+    weight = 200.10
+    println(weight)
+
+    // Write your car variables below
+    var paintColor = "Blueprint"
+    val make = " Toyota"
+    var model = "Corolla"
+    var year = 2021
+    var isManualTransmission = true
+    val seats = 21.43
+
+}
